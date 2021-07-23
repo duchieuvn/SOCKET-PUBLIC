6a8c526789768d52834301625738d3634ed718ec
Duc Hieu <tranthaiduchieu@gmail.com>
2021-07-23 20:03:52 +0700

    complete socket2

--- LEARN/socket2/server2.py
+++ LEARN/socket2/server2.py
@@ -1,9 +1,23 @@
 import socket 
 
 #192.168.1.119
-HOST = "127.0.0.1" #loopback
+HOST = "192.168.1.119" 
 SERVER_PORT = 65432 
 FORMAT = "utf8"
+
+def recvList(conn):
+    list = []
+
+    item = conn.recv(1024).decode(FORMAT)
+
+    while (item != "end"):
+        
+        list.append(item)
+        #response
+        conn.sendall(item.encode(FORMAT))
+        item = conn.recv(1024).decode(FORMAT)
+    
+    return list
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
@@ -15,22 +29,34 @@ print("SERVER SIDE")
 
 print("server:", HOST, SERVER_PORT)
 print("Waiting for Client")
-conn, addr = s.accept()
 
-print("Client", addr, "connected")
+try:
+    conn, addr = s.accept()
+    print("Client", addr, "connected")
+    print("conn:",conn.getsockname())
 
-msg = None
-while msg != "x":
-    try:
-        msg = int(conn.recv(1024))
-        print("client ",addr, "says", msg)
-        print("conn:",conn.getsockname())
     
-    except:
-        print("Error")
+
+    msg = None
+    while (msg != "x"):
+        msg = conn.recv(1024).decode(FORMAT)
+        print("client ",addr, "says", msg)
+        if (msg == "list"):
+            #response
+            conn.sendall(msg.encode(FORMAT))
+            list = recvList(conn)
+
+            print("received: ")
+            print(list)
+        
+    
+
+except:
+    print("Error")
+    
 
 
-
+print("End")
 input()
 conn.close()
 
