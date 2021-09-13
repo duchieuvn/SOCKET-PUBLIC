0017e00d02dc1b554caa9ffdaeaad8607781fcd1
Duc Hieu <tranthaiduchieu@gmail.com>
2021-09-13 15:00:08 +0700

    working on GUI

--- LEARN/socket5/socket5.py
+++ LEARN/socket5/socket5.py
@@ -7,12 +7,37 @@ class App(tk.Tk):
         tk.Tk.__init__(self)
 
         self.geometry("500x200")
-                
-        label_username = tk.Label(self, text='username')
-        btn_login = tk.Button(self, text='login')
+        
+        #
+        container = tk.Frame(self)
+        container.configure(bg="red")
+        container.pack(side="top", fill = "both", expand = True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-        label_username.pack()
-        btn_login.pack()
+
+        frame = StartPage(container)
+        frame.grid(row=0, column=0, sticky="nsew")
+        frame.tkraise()
+
+
+        
+        
+
+
+class StartPage(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.configure(bg="bisque2")
+
+        label_title = tk.Label(self, text="LOG IN")
+        label_user = tk.Label(self, text="username ")
+        label_pswd = tk.Label(self, text="password ")
+
+        label_title.pack()
+        label_user.pack()
+        label_pswd.pack()
+
 
 app = App()
 app.mainloop()
